5984abcfb4c26870567ff81655d37e32a293a316
doggy-tao <rtzhang@stu.jnu.edu.cn>
2024-12-11 20:20:02 +0800

    first commit

--- config.py
+++ config.py
@@ -5,19 +5,19 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 # Authentication for user filing issue (must have read/write access to repository to add issue to)
-USERNAME = ''
-TOKEN = ''
+USERNAME = 'eggman-1024'
+TOKEN = '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'
 
 # The repository to add this issue to
-REPO_OWNER = ''
-REPO_NAME = ''
+REPO_OWNER = 'eggman-1024'
+REPO_NAME = 'hello-arXiv'
 
 # Set new submission url of subject
 NEW_SUB_URL = 'https://arxiv.org/list/cs/new'
 
 # Keywords to search
-KEYWORD_LIST = ["", ]
+KEYWORD_LIST = ["object detection", "point cloud", "tracking"]
 
 
-OPENAI_API_KEYS = ['', ]
+OPENAI_API_KEYS = ['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', ]
 LANGUAGE = "zh"  # zh | en
